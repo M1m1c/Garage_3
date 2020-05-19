8a894ac12f51e0169ff87100ddfd8abdc1a1913d
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-05-19 11:35:41 +0200

    Added VehicleViewModel and ToVehicleViewModel method to controller

---
+++ Garage_3/Models/ViewModel/VehicleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_3.Models.ViewModel
+{
+    public class VehicleViewModel
+    {
+
+        public string RegNum { get; set; }
+
+        public string VColor { get; set; }
+
+        public string VType { get; set; }
+
+        public string OwnerUserName { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+    }
+}
