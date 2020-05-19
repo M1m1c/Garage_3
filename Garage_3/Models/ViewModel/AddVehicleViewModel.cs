261cf27bd132571c72c65fe851f1fadc59c74744
Jörgen Forsberg <j.forsberg65@outlook.com>
2020-05-19 21:18:56 +0200

    Display name & ErrorMessage translated

--- Garage_3/Models/ViewModel/AddVehicleViewModel.cs
+++ Garage_3/Models/ViewModel/AddVehicleViewModel.cs
@@ -11,21 +11,27 @@ namespace Garage_3.Models.ViewModel
     {
         [Required]
         [Remote(action: "RegNumExists", controller: "Vehicles", HttpMethod = "POST", ErrorMessage = "Registreringsnummer finns redan")]
+        [Display(Name = "Regnummer")]
         public string RegNum { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Ange antal hjul")]
+        [Display(Name = "Antal hjul")]
         public int Wheels { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Ange modell")]
+        [Display(Name = "Modell")]
         public string Model { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ange fabrikat")]
+        [Display(Name = "Fabrikat")]
         public string Brand { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ange färg")]
+        [Display(Name = "Färg")]
         public string ColorName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Ange fordonstyp")]
+        [Display(Name = "Fordonstyp")]
         public string VehicleType { get; set; }
     }
 }
