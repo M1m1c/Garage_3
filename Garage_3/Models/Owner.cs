bb0b01aa88e8e4fefedfb8028f7f630625762a78
Jörgen Forsberg <j.forsberg65@outlook.com>
2020-05-19 11:19:22 +0200

    Data annotations translations

--- Garage_3/Models/Owner.cs
+++ Garage_3/Models/Owner.cs
@@ -11,17 +11,25 @@ namespace Garage_3.Models
     public class Owner
     {
         [Key]
+        [Display(Name = "Medlemsnummer")]
         public int MemberNumber { get; set; }
 
         [Remote(action: "UserNameExists",controller:"Vehicles",HttpMethod ="POST",ErrorMessage ="Användar Namnet finns redan")]
+        [Display(Name = "Användarnamn")]
         public string UserName { get; set; }
+        
+        [Display(Name = "Förnamn")]
         public string FirstName { get; set; }
+        
+        [Display(Name = "Efternamn")]
         public string LastName { get; set; }
 
         [Remote(action: "EmailExists", controller: "Vehicles", HttpMethod = "POST", ErrorMessage = "Emailadressen används redan")]
+        [Display(Name = "E-post")]
         public string Email { get; set; }
 
         [Remote(action: "PhoneExists", controller: "Vehicles", HttpMethod = "POST", ErrorMessage = "Telefonnumret används redan")]
+        [Display(Name = "Telefon")]
         public string Telephone { get; set; }
 
         //Navigation Property
