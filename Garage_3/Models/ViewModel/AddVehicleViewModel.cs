9a7666d709b85cebb2cef6d78e7c5f021448bc03
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-05-19 15:37:08 +0200

    created Add vehicle View Model

---
+++ Garage_3/Models/ViewModel/AddVehicleViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage_3.Models.ViewModel
+{
+    public class AddVehicleViewModel
+    {
+        [Required]
+        [Remote(action: "RegNumExists", controller: "Vehicles", HttpMethod = "POST", ErrorMessage = "Registreringsnummer finns redan")]
+        public string RegNum { get; set; }
+        
+        [Required]
+        public int Wheels { get; set; }
+        
+        [Required]
+        public string Model { get; set; }
+
+        [Required]
+        public string Brand { get; set; }
+
+        [Required]
+        public string ColorName { get; set; }
+
+        [Required]
+        public string VehicleType { get; set; }
+    }
+}
