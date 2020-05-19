867ed55da6bf1a72ecf730828d462ffd32f43532
Jörgen Forsberg <j.forsberg65@outlook.com>
2020-05-19 11:48:11 +0200

    Översättning OwnerViewModel

--- Garage_3/Models/ViewModel/OwnerViewModel.cs
+++ Garage_3/Models/ViewModel/OwnerViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace Garage_3.Models.ViewModel
 {
     public class OwnerViewModel
     {
+        [Display(Name = "Medlemsnummer")]
         public int MemberNumber { get; set; }
+        
+        [Display(Name = "Förnamn")]
         public string FirstName { get; set; }
+        
+        [Display(Name = "Efternamn")]
         public string LastName { get; set; }
+        
+        [Display(Name = "Användarnamn")]
         public string UserName { get; set; }
 
 
