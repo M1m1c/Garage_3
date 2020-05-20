68b297fef2e038ff66a78e462c47c255bc8a9908
ludvi <ludvi@DESKTOP-6U5E7KR>
2020-05-20 10:45:02 +0200

    Fixed Details include

--- Garage_3/Controllers/VehiclesController.cs
+++ Garage_3/Controllers/VehiclesController.cs
@@ -44,13 +44,13 @@ namespace Garage_3.Controllers
                 return NotFound();
             }
 
-            var vehicle = await _context.Vehicle
+            var vehicle = await _context.Vehicle.Include(v => v.Color).Include(v => v.VehicleType).Include(v => v.Owner)
                 .FirstOrDefaultAsync(m => m.RegNum == id);
             if (vehicle == null)
             {
                 return NotFound();
             }
-
+            
             return View(vehicle);
         }
         public async Task<IActionResult> Profile(int? id)
